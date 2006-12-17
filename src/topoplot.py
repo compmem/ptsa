59676fad888d50c09c016668da9a1ab5fee2a162
ctw <devnull@localhost>
2006-12-17 20:15:57 +0000

    first version; only draws outline of head

---
+++ src/topoplot.py
@@ -0,0 +1,54 @@
+from numpy import *
+from pylab import *
+from matplotlib.patches import Circle
+
+def topoplot():
+    headCenter = (0,0)
+    headRad = 0.5
+    headLineWidth = 3
+    head = Circle(headCenter,headRad,fill=False,linewidth=headLineWidth)
+    
+    noseLineWidth = earLineWidth = 2
+    noseWidth = 0.18*headRad
+    # Distance from the center of the head to the point where the nose touches the outline of the head: 
+    noseDist = math.cos(math.asin((noseWidth/2)/headRad))*headRad
+    # Distance from the center of the head to the tip of the nose:
+    noseTipDist = 1.15*headRad
+    nose = Line2D([-noseWidth/2,0,noseWidth/2],[noseDist,noseTipDist,noseDist],color='black',linewidth=noseLineWidth,solid_joinstyle='round',solid_capstyle='round')
+    
+    q = .04 # ear lengthening
+    earX = array([.497-.005,.510,.518,.5299,.5419,.54,.547,.532,.510,.489-.005])
+    earY = array([q+.0555,q+.0775,q+.0783,q+.0746,q+.0555,-.0055,-.0932,-.1313,-.1384,-.1199])
+    earRight = Line2D(earX,earY,color='black',linewidth=earLineWidth,solid_joinstyle='round',solid_capstyle='round')
+    earLeft = Line2D((earX*-1),earY,color='black',linewidth=earLineWidth,solid_joinstyle='round',solid_capstyle='round')
+    
+    
+    a=subplot(1,1,1, aspect='equal')
+    xlim(-1, 1)
+    ylim(-1, 1)
+    a.add_artist(head)
+    a.add_artist(nose)
+    a.add_artist(earRight)
+    a.add_artist(earLeft)
+    show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
