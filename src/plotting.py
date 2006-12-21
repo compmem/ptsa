cd8e7bc71d1cb22f3d4768ddc49751ddad10afce
ctw <devnull@localhost>
2006-12-21 14:52:24 +0000

    it's a mess right now ...

--- src/plotting.py
+++ src/plotting.py
@@ -3,6 +3,7 @@ from pylab import *
 from matplotlib.patches import Circle
 import csv
 from helper import pol2cart
+from griddata import griddata
 
 def topoplot():
     """Plot a topographic map of the scalp in a 2-D circular view (looking down at the top of the head).
@@ -43,15 +44,79 @@ def topoplot():
     ########################################################
     
     x,y = getElectrodeCoords(headRad)
+    
+    #xi = linspace(min(x),max(x),10)
+    #yi = linspace(min(y),max(y),10)
+    
+    
+    #x = random.uniform(-2,2,100);  y = random.uniform(-2,2,100)
+    #z = x*exp(-x**2-y**2)
+    z = rand(129)
+    
+    # x, y, and z are now vectors containing nonuniformly sampled data.
+    # Define a regular grid and grid data to it.
+    nx = 51; ny = 41
+    xi, yi = meshgrid(linspace(-2,2,nx),linspace(-2,2,ny))
+    # masked=True mean no extrapolation, output is masked array.
+    zi = griddata(x,y,z,xi,yi,masked=True)
+    
+    #nx = 50; ny = 50
+    #xi, yi = meshgrid(linspace(min(x),max(x),nx),linspace(min(y),max(y),ny))
+    
+    #Xi,Yi,Zi =
+    #zi = griddata(y,x,rand(129),yi,xi,masked=True)
+    #CS = contour(xi,yi,zi,15,linewidths=0.5,colors=['k'])
+    CS = contourf(xi,yi,zi,15,cmap=cm.jet)
+    #test=imshow(zi)
+    
+    print 'min/max = ',min(zi.compressed()),max(zi.compressed())
 
-    a=subplot(1,1,1, aspect='equal')
+    #ax = axes([0.1,0.1,0.75,0.75])
+    # Contour the gridded data, plotting dots at the nonuniform data points.
+    # CS = p.contour(xi,yi,zi,15,linewidths=0.5,colors=['k'])
+    # CS = p.contourf(xi,yi,zi,15,cmap=p.cm.jet)
+    # cax = p.axes([0.875, 0.1, 0.05, 0.75]) # setup colorbar axes
+    # p.colorbar(tickfmt='%4.2f', cax=cax) # draw colorbar
+    # p.axes(ax)  # make the original axes current again
+    # p.scatter(x,y,marker='o',c='b',s=5)
+    # p.xlim(-1.9,1.9)
+    # p.ylim(-1.9,1.9)
+    # p.title('griddata test (%d points)' % npts)
+    # p.savefig('griddata.png')
+    # p.show()
+    
+    
+    
+    #X, Y = meshgrid(x,y)
+    #Z1 = bivariate_normal(X, Y, 1.0, 1.0, 0.0, 0.0)
+    #Z2 = bivariate_normal(X, Y, 1.5, 0.5, 1, 1)
+    # difference of Gaussians
+    #Z = random.rand(129,129) #10.0 * (Z2 - Z1)
+    
+    
+    
+    # Create a simple contour plot with labels using default colors.  The
+    # inline argument to clabel will control whether the labels are draw
+    # over the line segments of the contour, removing the lines beneath
+    # the label
+    #figure()
+    #im = imshow(Z, interpolation='bilinear', origin='lower',
+    #        cmap=cm.gray)#, extent=(-1,3,-2,2))
+    
+    
+    
+    
+    plot(x,y,'bo')
+    
+    #a=subplot(1,1,1, aspect='equal')
     xlim(-(headRad*2)+headCenter[0], (headRad*2)+headCenter[0])
     ylim(-(headRad*2)+headCenter[1], (headRad*2)+headCenter[1])
-    a.add_artist(head)
-    a.add_artist(nose)
-    a.add_artist(earRight)
-    a.add_artist(earLeft)
-    plot(x,y,'bo')
+    #a.add_artist(head)
+    #a.add_artist(nose)
+    #a.add_artist(earRight)
+    #a.add_artist(earLeft)
+    #plot(x,y,'bo')
+    #a.add_artist(CS)
     show()
 
 
