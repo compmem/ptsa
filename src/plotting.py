447c3441cd33ef15884300c62a486b3e6aaecf49
ctw <devnull@localhost>
2006-12-21 00:56:52 +0000

    cosmetic changes

--- src/plotting.py
+++ src/plotting.py
@@ -8,11 +8,23 @@ def topoplot():
     """Plot a topographic map of the scalp in a 2-D circular view (looking down at the top of the head).
     Nose is at top of plot; left is left; right is right. More to come ...."""
     
+    ########################################################
+    # Set up head:
+    #
+    # Basics:
+    # Coordinates of middle of head:
     headCenter = (0,0)
+    #
+    # Angle of direction where the nose points in degrees:
+    # 0 deg is top, 90 deg is right, etc.
+    noseDeg = 0 # not yet used!
+    #
+    # Outline:
     headRad = 0.5 # radius of head
     headLineWidth = 3
     head = Circle(headCenter,headRad,fill=False,linewidth=headLineWidth)
-    
+    #
+    # Nose:    
     noseLineWidth = earLineWidth = 2
     noseWidth = 0.18*headRad
     # Distance from the center of the head to the point where the nose touches the outline of the head: 
@@ -20,12 +32,15 @@ def topoplot():
     # Distance from the center of the head to the tip of the nose:
     noseTipDist = 1.15*headRad
     nose = Line2D([-noseWidth/2+headCenter[0],0+headCenter[0],noseWidth/2+headCenter[0]],[noseDist+headCenter[1],noseTipDist+headCenter[1],noseDist+headCenter[1]],color='black',linewidth=noseLineWidth,solid_joinstyle='round',solid_capstyle='round')
-    
+    #
+    # Ears:
     q = .04 # ear lengthening
     earX = array([.497-.005,.510,.518,.5299,.5419,.54,.547,.532,.510,.489-.005])*(headRad/0.5)#+headCenter[0]
     earY = array([q+.0555,q+.0775,q+.0783,q+.0746,q+.0555,-.0055,-.0932,-.1313,-.1384,-.1199])*(headRad/0.5)#+headCenter[1]
     earRight = Line2D(earX+headCenter[0],earY+headCenter[1],color='black',linewidth=earLineWidth,solid_joinstyle='round',solid_capstyle='round')
     earLeft = Line2D(headCenter[0]-earX,earY+headCenter[1],color='black',linewidth=earLineWidth,solid_joinstyle='round',solid_capstyle='round')
+    #
+    ########################################################
     
     x,y = getElectrodeCoords()
     
