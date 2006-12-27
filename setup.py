c74763ea466976793d883756a05d2846aef6c48b
per <devnull@localhost>
2006-12-27 21:57:42 +0000

    *** empty log message ***

--- setup.py
+++ setup.py
@@ -9,13 +9,10 @@ import sys
 
 setup(name='pyeeg', 
       version='0.0.1', ### MAKE SURE THIS MATCHES src/version.py !!
-      package_dir={"pyeeg":"code"},
+      package_dir={"pyeeg":"src"},
       packages=['pyeeg'],
       author=['Per B. Sederberg, Christoph T. Weidemann'],
       maintainer=['Per B. Sederberg'],
       maintainer_email=['psederberg@gmail.com'],
-      url=[''],
-      ext_modules=ext_modules, 
-      cmdclass = {'build_ext': build_ext}, 
-      data_files=data_files)
+      url=[''])
 
