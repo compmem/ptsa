0e0aac20b1f8dc3851143baff367c452a826daa4
per <devnull@localhost>
2007-03-10 18:05:55 +0000

    Minor change to __init__

--- src/__init__.py
+++ src/__init__.py
@@ -3,10 +3,10 @@ PyEEG - The Python EEG toolbox.
 """
 
 
-from data import DataArray,DataWrapper,Events,InfoArray,RawBinaryEEG,createEventsFromMatFile
+from data import DataArray,DataDict,DataWrapper,Events,RawBinaryEEG,createEventsFromMatFile
 from filter import buttfilt, decimate, filtfilt
 from plotting import topoplot
-from wavelet import tfPhasePow,phasePow1d
+from wavelet import tfPhasePow,phasePow1d,phasePow2d
 from version import versionAtLeast,versionWithin
 
 #__all__ = [data,filter,plotting,wavelet]
