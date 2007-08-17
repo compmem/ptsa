e4e33f83270a495f345cb2b91afad89776f15ce2
Per B. Sederberg <psederberg@gmail.com>
2007-08-16 23:15:35 -0400

    Base level of numpy testing framework added based a bit on nipy.

--- src/__init__.py
+++ src/__init__.py
@@ -11,3 +11,35 @@ from version import versionAtLeast,versionWithin
 
 #__all__ = [data,filter,plotting,wavelet]
 
+packages = ('pyeeg',
+            'pyeeg.tests')
+
+def _test(method, level, verbosity, flags):
+    """
+    Run test suite with level and verbosity.
+
+        level:
+          None           --- do nothing, return None
+          < 0            --- scan for tests of level=abs(level),
+                             don't run them, return TestSuite-list
+          > 0            --- scan for tests of level, run them,
+                             return TestRunner
+
+        verbosity:
+          >= 0           --- show information messages
+          > 1            --- show warnings on missing tests
+    """
+    from numpy.testing import NumpyTest, importall
+    #from neuroimaging.utils.testutils import set_flags
+    #set_flags(flags)
+    importall('pyeeg')
+    return getattr(NumpyTest(), method)(level, verbosity=2)
+
+def test(level=1, verbosity=1, flags=[]):
+    _test('test', level=level, verbosity=verbosity, flags=flags)
+test.__doc__ = "Using NumpyTest test method.\n"+_test.__doc__
+
+def testall(level=1, verbosity=1, flags=[]):
+    _test('testall', level=level, verbosity=verbosity, flags=flags)
+testall.__doc__ = "Using NumpyTest testall method.\n"+_test.__doc__
+
---
+++ src/tests/test_data.py
@@ -0,0 +1,32 @@
+from numpy.testing import NumpyTest, NumpyTestCase
+
+class test_Template(NumpyTestCase):
+
+    def setUp(self): pass
+        #print "TestCase initialization..."
+
+    def test_foo(self): pass
+        #print "testing foo"
+
+    def test_bar(self): pass
+        #print "testing bar"
+      
+
+if __name__ == '__main__':
+    NumpyTest.main()
+
+
+# test RawBinaryEEG
+
+# load data from file
+
+# with resampling
+
+# without resampling
+
+# make sure the time range is correct
+
+# make sure we get the expected number of samples
+
+
+
