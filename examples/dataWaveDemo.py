bdd0f491358b0cc12c982376d4876d50de3003b6
Per B. Sederberg <psederberg@gmail.com>
2008-01-30 10:25:40 -0500

    BF: Updated example to use new TimeSeries.

--- examples/dataWaveDemo.py
+++ examples/dataWaveDemo.py
@@ -19,25 +19,25 @@ ev = createEventsFromMatFile('/home1/per/eeg/free/CH012/events/events.mat')
 # in the call to tsPhasePow
 freqs = range(2,81,2)
 chan = 27
-durationMS = 2500
-offsetMS = -500
-bufferMS = 1000
+dur = 2.5
+offset = .500
+buf = 1.000
 resampledRate = 200
 filtFreq = [58.0,62.0]
 
 # load the eeg data
 print "Loading EEG data..."
 rEEG = ev.select(ev['recalled']==1).get_data(chan,
-                                             durationMS,
-                                             offsetMS,
-                                             bufferMS,
+                                             dur,
+                                             offset,
+                                             buf,
                                              resampledRate,
                                              filtFreq=filtFreq,
                                              keepBuffer=True)
 nEEG = ev.select(ev['recalled']==0).get_data(chan,
-                                             durationMS,
-                                             offsetMS,
-                                             bufferMS,
+                                             dur,
+                                             offset,
+                                             buf,
                                              resampledRate,
                                              filtFreq=filtFreq,
                                              keepBuffer=True)
@@ -66,7 +66,7 @@ pylab.figure(fig)
 pylab.plot(rEEG['time'],rEEG.aggregate('event',N.mean).data,'r')
 pylab.plot(nEEG['time'],nEEG.aggregate('event',N.mean).data,'b')
 pylab.legend(('Recalled','Not Recalled'))
-pylab.xlabel('Time (ms)')
+pylab.xlabel('Time (s)')
 pylab.ylabel('Voltage (mV)')
 
 # power spectrum
@@ -83,7 +83,7 @@ fig+=1
 pylab.figure(fig)
 pylab.contourf(rPow['time'],rPow['freq'],rPow.data-nPow.data)
 pylab.colorbar()
-pylab.xlabel('Time (ms)')
+pylab.xlabel('Time (s)')
 pylab.ylabel('Frequency (Hz)')
 pylab.title('SME (diff in power) for channel %d' % (chan))
 
