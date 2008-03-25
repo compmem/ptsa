0d37aeb1c61b83ca5c1a2b8590d4530706d13bb1
Christoph Weidemann <ctw@kahamac1.(none)>
2008-03-24 23:06:16 -0400

    RF: preparation for wavelet.py RF

---
+++ ptsa/fixed_scipy/__init__.py
@@ -0,0 +1,12 @@
+#emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*-
+#ex: set sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See the COPYING file distributed along with the PTSA package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+
+"""Functions that contain a bug in the latest scipy stable release are copied here (fixed) until they are fixed in the stable release."""
+
+from wavelets import morlet
---
+++ ptsa/fixed_scipy/wavelets.py
@@ -0,0 +1,47 @@
+# scipy.signal.wavelets.morlet
+def morlet(M, w=5.0, s=1.0, complete=True):
+    """Complex Morlet wavelet.
+
+    Parameters
+    ----------
+    M : int
+        Length of the wavelet.
+    w : float
+        Omega0
+    s : float
+        Scaling factor, windowed from -s*2*pi to +s*2*pi.
+    complete : bool
+        Whether to use the complete or the standard version.
+
+    Notes:
+    ------
+    The standard version:
+        pi**-0.25 * exp(1j*w*x) * exp(-0.5*(x**2))
+
+        This commonly used wavelet is often referred to simply as the
+        Morlet wavelet.  Note that, this simplified version can cause
+        admissibility problems at low values of w.
+
+    The complete version:
+        pi**-0.25 * (exp(1j*w*x) - exp(-0.5*(w**2))) * exp(-0.5*(x**2))
+
+        The complete version of the Morlet wavelet, with a correction
+        term to improve admissibility. For w greater than 5, the
+        correction term is negligible.
+
+    Note that the energy of the return wavelet is not normalised
+    according to s.
+
+    The fundamental frequency of this wavelet in Hz is given
+    by f = 2*s*w*r / M where r is the sampling rate.
+
+    """
+    x = linspace(-s*2*pi,s*2*pi,M)
+    output = exp(1j*w*x)
+    
+    if complete:
+        output -= exp(-0.5*(w**2))
+    
+    output *= exp(-0.5*(x**2)) * pi**(-0.25)
+    
+    return output
