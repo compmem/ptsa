ed55be3b8a968eee533428c94f478fb08ac3405e
Per B. Sederberg <psederberg@gmail.com>
2011-03-24 17:38:59 -0400

    BK: Almost done with first pass on align

--- ptsa/data/align.py
+++ ptsa/data/align.py
@@ -11,26 +11,81 @@
 # methods for aligning events and eeg data
 
 import os
+import csv
+import numpy as np
 
-def clean_pyepl_eeglog(logfile):
+from edfwrapper import EdfWrapper
+
+def load_pyepl_eeg_pulses(logfile):
+    # open and read each line
+    reader = csv.reader(open(logfile,'rU'),dialect=csv.excel_tab)
+    pulses = []
+    for row in reader:
+        if row[2] == 'UP':
+            pulses.append(long(row[0]))
+    return np.asarray(pulses)
+
+def find_needle_in_haystack(needle, haystack, maxdiff):
+    """
+    """
+    nlen = len(needle)
+    found = False
+    for i in range(len(haystack)-nlen):
+        if (haystack[i:i+nlen] - needle).max() < maxdiff:
+            found = True
+            break
+    if not found:
+        i = None
+    return i
+
+def align_edf_pyepl(edffile, eeglog, events, annot_id='S255', 
+                    channel_for_sr=0, 
+                    window=100, thresh_ms=10):
+    """
+    """
+    # create the edf wrapper
+    ew = EdfWrapper(edffile)
+
+    # load clean pyepl eeg log
+    pulse_ms = load_pyepl_eeg_pulses(eeglog)
+
+    # load annotations from edf
+    annot = ew.get_annotations()
+
+    # convert seconds to ms for annot_ms
+    annot_ms = annot[annot['annotations']==annot_id]['onsets'] * 1000
+
+    # pick beginning and end (needle in haystack)
+    for i in xrange(len(annot_ms)-window):
+        s_ind = find_needle_in_haystack(np.diff(annot_ms[i:i+window]),np.diff(pulse_ms),thresh_ms)
+        if not s_ind is None:
+            break
+    if s_ind is None:
+        raise ValueError("Unable to find a start window.") # get better error here
+    start_annot_vals = annot_ms[i:i+window]
+    start_pulse_vals = pulse_ms[s_ind:s_ind+window]
+
+    for i in xrange(len(annot_ms)-window):
+        e_ind = find_needle_in_haystack(np.diff(annot_ms[::-1][i:i+window]),np.diff(pulse_ms[::-1]),thresh_ms)
+        if not e_ind is None:
+            break
+    if e_ind is None:
+        raise ValueError("Unable to find a end window.") # get better error here
+
+    # correct the end ind 
+    e_ind = len(pulse_ms) - e_ind - window
+
+    i = len(annot_ms) - i - window
+    end_annot_vals = annot_ms[i:i+window]
+    end_pulse_vals = pulse_ms[e_ind:e_ind+window]
+
+    # fit line with regression
+    x = np.r_[start_pulse_vals,end_pulse_vals]
+    y = np.r_[start_annot_vals,end_annot_vals]
+    m,c = np.linalg.lstsq(np.vstack([x-x[0],np.ones(len(x))]).T, y)[0]
+    c = c - x[0]*m
+
+    # add esrc and eoffset to the Events instance
+
     pass
-
-
-def align_edf_pyepl(edffile, eeglog, events):
-    pass
-
-# load and clean pyepl eeg log
-# extract up times for pulse_ms
-
-# load annotations from edf
-# convert seconds to ms for annote_ms
-
-# pick beginning and end (needle in haystack)
-
-
-# fit line with regress
-
-
-# add esrc and eoffset to the Events instance
-
 
