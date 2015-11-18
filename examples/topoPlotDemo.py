847000848b98d691df07bb2c62ebafb42b85671d
Christoph T. Weidemann <ctw@cogsci.info>
2015-11-17 22:33:17 -0500

    BF: some fixes to the topoplot demo

--- examples/topoPlotDemo.py
+++ examples/topoPlotDemo.py
@@ -1,5 +1,5 @@
 from pylab import loadtxt, rand, figure, xlim, ylim, show
-from ptsa.plotting.topoplot import topoplot
+from ptsa.plotting.topo import topoplot
 
 def getElecs():
     # read in testLocs.dat that was generated in Matlab as follows:
@@ -35,20 +35,7 @@ topoplot(sensors=els)
 
 fig+=1
 figure(fig)
-topoplot(sensors=els,colors=[None,'black','black'])
-#show()
-
-fig+=1
-figure(fig)
-topoplot(values=toPlot,sensors=els,colors=['black',None,'black'])
-
-fig+=1
-figure(fig)
-topoplot(sensors=els,values=toPlot,colors=['black','black',None])
-
-fig+=1
-figure(fig)
-topoplot(sensors=els,values=toPlot,colors=[None,None,'black'])
+topoplot(sensors=els,values=toPlot)
 
 grid=100
 fig+=1
@@ -60,18 +47,4 @@ topoplot(center=(8.5,0),radius=2,sensors=els,values=toPlot,resolution=grid)
 xlim(-0.5,12)
 #axis('on')
 #show()
-
-fig+=1
-figure(fig)
-topoplot(center=(0,0),nose_dir=-45,sensors=els,values=toPlot,colors=['black',None,'black'],resolution=grid)
-topoplot(center=(0,2),nose_dir=-135,sensors=els,values=toPlot,colors=['black',None,'black'],resolution=grid)
-topoplot(center=(2,0),nose_dir=135,sensors=els,values=toPlot,colors=['black',None,'black'],resolution=grid)
-topoplot(center=(2,2),nose_dir=45,sensors=els,values=toPlot,colors=['black',None,'black'],resolution=grid)
-xlim(-1,3)
-ylim(-1,3)
 #show()
-
-#topoplot
-
-
-show()
